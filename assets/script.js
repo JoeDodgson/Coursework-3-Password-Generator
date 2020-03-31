063c252378bcd79ed3785dd165713f644ac4f872
Joe Dodgson <Joe.Dodgson@arup.com>
2020-03-31 14:15:22 +0100

    Put css and js into assets subfolder

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,9 @@
-// Assignment Code
+// Assignment of variables
 var generateBtn = document.querySelector("#generate");
-
 var passwordLength = "";
-
 var charsLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
 var charsUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "Q", "X", "Y", "Z"];
-
 var charsNumeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
 var charsSpecial = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "{", "}", "[", "]", "|", "\\", "/", ":", ";", '"', "'", "<", ">", ",", ".", "?"];
 
 // Write password to the #password input
