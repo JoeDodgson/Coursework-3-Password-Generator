2fc9e01ffebfc2dd719c49ead44f44461b25bf95
Joe Dodgson <Joe.Dodgson@arup.com>
2020-03-31 13:53:48 +0100

    Populated readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Coursework-3-Password-Generator
 Coding Bootcamp Coursework 3 - Password Generator
+
+A web page which generates a random password for a user based on 5 criteria. Through a series of prompts, the user selects password length (8 to 128 characters) and selects which characters should be included in the generated password (lower case, upper case, numeric and special characters)
+
+No installation required
+
+To generate a new password, click the 'Generate password' button and follow the on screen prompts
+
+Contribution by Joe Dodgson only
+
+Credit to my teachers for answering my many questions!
+
+License: Open source
--- script.js
+++ script.js
@@ -19,11 +19,11 @@ function writePassword() {
   var charsPossibleCurrent = 0;
   var passwordText = "";
 
-  //********************************Clear out any passwords which are already displaying
+  //Clear out any passwords which are already displayed in the text area
   document.getElementById("password").innerHTML = "";
 
-  // Welcome message. OK=continue. Cancel=close.
-  var continueBool = confirm("Welcome to Joe's password generator. Press 'OK' to continue. Or press 'Cancel' to close");
+  //Welcome message. OK=continue. Cancel=close.
+  var continueBool = confirm("Welcome to Joe's password generator.\nClick 'OK' to continue. Or click 'Cancel' to close");
   
   if (continueBool == false) {
     alert("Sorry you didn't want to generate a password. If you change your mind, click the 'Generate Password' button")
@@ -33,7 +33,7 @@ function writePassword() {
   }
 
   //Password length prompt
-  var passwordLength = prompt("Please enter a password length between 8 and 128 characters and click 'OK' to confirm");
+  var passwordLength = prompt("Please enter a password length between 8 and 128 characters.\nClick OK to confirm your selection");
   
   //Change password length to an integer
   passwordLength = parseFloat(passwordLength);
@@ -47,7 +47,7 @@ function writePassword() {
   }
 
   //Ask whether to include Lower case
-  var includeLower = confirm("Would you like to include Lower case characters in your password? Ok = yes. Cancel = no.");
+  var includeLower = confirm("Would you like to include Lower case characters in your password?\nOK = yes. Cancel = no.");
 
   //Store the current length of the charsPossible array
   charsPossibleCurrent = charsPossible.length;
@@ -63,7 +63,7 @@ function writePassword() {
 
 
   //Ask whether to include Upper case
-  var includeUpper = confirm("Would you like to include Upper case characters in your password? Ok = yes. Cancel = no.");
+  var includeUpper = confirm("Would you like to include Upper case characters in your password?\nOK = yes. Cancel = no.");
 
   //Store the current length of the charsPossible array
   charsPossibleCurrent = charsPossible.length;
@@ -79,7 +79,7 @@ function writePassword() {
 
 
   //Ask whether to include Numeric characters
-  var includeNumeric = confirm("Would you like to include Numeric characters in your password? Ok = yes. Cancel = no.")
+  var includeNumeric = confirm("Would you like to include Numeric characters in your password?\nOK = yes. Cancel = no.")
 
   //Store the current length of the charsPossible array
   charsPossibleCurrent = charsPossible.length;
@@ -95,7 +95,7 @@ function writePassword() {
 
 
   //Ask whether to include Special characters
-  var includeSpecial = confirm("Would you like to include Special characters in your password? Ok = yes. Cancel = no.")
+  var includeSpecial = confirm("Would you like to include Special characters in your password?\nOK = yes. Cancel = no.")
 
   //Store the current length of the charsPossible array
   charsPossibleCurrent = charsPossible.length;
@@ -132,35 +132,5 @@ function writePassword() {
   document.querySelector("textarea").setAttribute("style","border: solid #00cc00;");
 }
 
-//Activate the writePassword function when the button is clicked
+//Add event listener for 'generate password' button click. Activate writePassword function when clicked
 generateBtn.addEventListener("click", writePassword);
-
-// passwordText.value = password;
-// var password = generatePassword();
-// var passwordText = document.querySelector("#password");
-
-// Add event listener to generate button
-
-// -------------Task List---------------
-// DONE Confirm: Welcome to Joe's password generator. Press ok to continue
-// DONE Prompt: Please enter a chosen password length from 8 to 128 characters. Or press Cancel to close.
-// DONE   If: entered length is not between 8 and 128, then alert: you must enter a character length between 8 and 128, then return to prompt.
-// DONE Else: continue to next confirms
-// DONE Confirm: Would you like to include lower case characters in your password? Ok = yes. Cancel = no.
-// DONE If confirm 1 = yes, 
-// DONE For i = 0 to i < charsLower.length, charsPossible[i] = charsLower[i]
-// DONE Confirm: Would you like to include upper case characters in your password? Ok = yes. Cancel = no.
-// DONE For i = charsPossible.length to i < charsPossible + charsUpper.length, charsPossible[i] = charsLower[i]
-// DONE Confirm: Would you like to include numeric characters in your password? Ok = yes. Cancel = no.
-// DONE Confirm: Would you like to include special characters in your password? Ok = yes. Cancel = no.
-// DONE If: all 4 confirms = no, then alert: You must select at least one character type. then return back to first character confirm
-// DONE Else: continue to generate password
-// DONE Copy password out to the textbox
-// DONE Make the password selectable (and therefore you're able to copy it)
-// DONE Don't allow decimal password length
-// DONE When you click on the 'Generate password' button it clears out any text from the textarea
-// DONE Make responsive for different screen sizes
-// Make the text box change sizes based on the output text?
-// Use javascript to update CSS
-// 'Clean and polished user interface'
-//
